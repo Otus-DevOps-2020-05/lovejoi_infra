e81711a63c41fcb67c8ba31aeb58a725ad85e67a
Oleg Skogorev <oskogorev@gmail.com>
2020-06-26 11:50:29 +0400

    Modify files

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/bash
 # MongoDB installation and autostart script
+sudo apt-get install -y apt-transport-https
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo apt install iptables
-sudo iptables -A INPUT -p tcp --dport 2017 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 9292 -j ACCEPT
 sudo systemctl start mongod
 sudo systemctl enable mongod
-
-
