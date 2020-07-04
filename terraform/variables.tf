098d2955c6a4b08882673b5d4ab2ed2c0ca0b4df
Oleg Skogorev <oskogorev@gmail.com>
2020-07-04 19:17:27 +0400

    modify .gitignore file and add terraform directory

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable token {
+description = "Token"
+}
+variable cloud_id {
+description = "Cloud"
+}
+variable folder_id {
+description = "Folder"
+}
+variable zone {
+description = "Zone"
+# Значение по умолчанию
+default = "ru-central1-a"
+}
+variable image_id {
+description = "Image"
+}
+variable subnet_id {
+description = "Subnet"
+}
+variable private_key_path {
+description = "~/.ssh/ubuntu"
+} 
