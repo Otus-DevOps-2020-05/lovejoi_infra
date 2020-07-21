9cd631ddd61f7d4e4527fe29a80e910980d7df2b
Oleg Skogorev <oskogorev@gmail.com>
2020-07-21 08:56:36 +0400

    modify and add files for second task

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,4 +24,12 @@ description = "~/.ssh/ubuntu"
 variable public_key_path {
 description = "~/.ssh/ubuntu.pub"
 }
+variable app_disk_image {
+description = "Disk image for reddit app"
+default = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
  
