9772298c6985a673e0410d3849773f95bb3a2bc5
Oleg Skogorev <oskogorev@gmail.com>
2020-07-28 00:13:51 +0400

    Preparing files for modules

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,17 +1,17 @@
-resource "yandex_compute_instance" "app" {
-  name = "reddit-app"
-
+resource "yandex_compute_instance" "db" {
+  name = "reddit-db"
   labels = {
-    tags = "reddit-app"
+    tags = "reddit-db"
   }
+
   resources {
-    cores  = 1
+    cores  = 2
     memory = 2
   }
 
   boot_disk {
     initialize_params {
-      image_id = var.app_disk_image
+      image_id = var.db_disk_image
     }
   }
 
